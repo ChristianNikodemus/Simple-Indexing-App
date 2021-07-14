4ca0e043fc82c0000aa54c0b8108184de12672f0
ChristianNikodemus <christian.nikodemus@gmail.com>
2021-07-13 22:41:05 -0400

    Wrapped my pokemonList in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,36 @@
 // variable containing array of objects with data of different pokemon
-let pokemonList = [
-  {
-    name: 'Sandslash',
-    height: 1,
-    types: ['ground']
-  },
-  {
-    name: 'Noctowl',
-    height: 1.6,
-    types: ['flying','normal']
-  },
-  {
-    name: 'Tangrowth',
-    height: 2,
-    types: ['grass']
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: 'Sandslash',
+      height: 1,
+      types: ['ground']
+    },
+    {
+      name: 'Noctowl',
+      height: 1.6,
+      types: ['flying','normal']
+    },
+    {
+      name: 'Tangrowth',
+      height: 2,
+      types: ['grass']
+    }
+  ];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
-];
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
 
 pokemonList.forEach(function(pokemon) {
   if (pokemon.height >=2){

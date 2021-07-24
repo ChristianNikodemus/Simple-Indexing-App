62be860fcaf16ca63cd83a5d39b853e55f928f65
ChristianNikodemus <christian.nikodemus@gmail.com>
2021-07-24 09:32:45 -0400

    Defined fetch variable

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,10 @@
 let pokemonRepository = (function () {
 
   let pokemonList = [];
+
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  
+  const fetch = require("node-fetch");
 
   function add(pokemon) {
     pokemonList.push(pokemon);

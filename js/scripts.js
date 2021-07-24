fd37370a92c8cde95a967ef6e37108595fda4db3
ChristianNikodemus <christian.nikodemus@gmail.com>
2021-07-24 00:35:04 -0400

    Added variable for the pokemon API

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@
 let pokemonRepository = (function () {
 
   let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function add(pokemon) {
     pokemonList.push(pokemon);

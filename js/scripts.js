2b764c36e44e77a88488c32f163150edcaf5b214
ChristianNikodemus <christian.nikodemus@gmail.com>
2021-07-26 19:43:20 -0400

    Added the event listener to exit the modal if the user clicks outside the modal

--- js/scripts.js
+++ js/scripts.js
@@ -125,6 +125,15 @@ window.addEventListener('keydown', (e) => {
   }
 });
 
+modalContainer.addEventListener('click', (e) => {
+  // Since this is also triggered when clicking INSIDE the modal
+  // We only want to close if the user clicks directly on the overlay
+  let target = e.target;
+  if (target === modalContainer) {
+    hideModal();
+  }
+});
+
 document.querySelector('#show-modal').addEventListener('click', () => {
   showModal('Modal title', 'This is the modal content!');
 });

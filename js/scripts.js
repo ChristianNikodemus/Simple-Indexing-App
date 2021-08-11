a0ce5dc6e9c5a2691bce7f647d48329817d1540e
ChristianNikodemus <christian.nikodemus@gmail.com>
2021-08-10 23:57:13 -0400

    Changed group-list-item to have action

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,7 @@ let pokemonRepository = (function () {
     let button = document.createElement('button');
     $('button').addClass('my-button');
     $('button').addClass('btn');
-    $('listItem').addClass('group-list-item');
+    $('listItem').addClass('group-list-item-action');
     button.innerText = pokemon.name;
     listItem.appendChild(button);
     container.appendChild(listItem);
